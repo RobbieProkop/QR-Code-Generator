f31b6c8a7db7e40aafdde78f8bf4fef8f84fe48f
Robbie Prokop <robbieprokop@gmail.com>
2022-08-18 14:34:48 -0500

    added https onto urls

--- src/script.js
+++ src/script.js
@@ -1,19 +1,28 @@
 const form = document.getElementById("generate-form");
-const gr = document.getElementById("qrcode");
+const qr = document.getElementById("qrcode");
 
 const onGenerateSubmit = (e) => {
   e.preventDefault();
 
   const url = document.getElementById("url").value;
   const size = document.getElementById("size").value;
+  const https = "https://";
 
   !url ? alert("Please Enter a URL") : showSpinner(),
     setTimeout(() => {
       hideSpinner();
+      if (!url.includes("http://" || "https://")) {
+        const newUrl = https.concat(url);
+        console.log(newUrl);
+        generateQRCode(newUrl, size);
+      } else {
+        generateQRCode(url, size);
+      }
     }, 1000);
 };
 
 const generateQRCode = (url, size) => {
+  console.log("called");
   const qrcode = new QRCode("qrcode", {
     text: url,
     width: size,
